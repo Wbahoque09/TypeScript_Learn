a6e0db39c050bd6e5da86e4e2e3ef4461ff549ed
Will_09 <wbahoque71@gmail.com>
2023-05-25 17:31:17 -0500

    Se empiezan a manejar los tipos de datos en typescript

--- TypeScript-HolaMundo/src/index.ts
+++ TypeScript-HolaMundo/src/index.ts
@@ -7,3 +7,29 @@ mensaje = "Puerco feliz";
 
 mensaje = "chao mundo";
 console.log(mensaje);
+
+// Tipos nativos
+/**
+ * Tipos de JS
+ * number
+ * string
+ * boolean
+ * null
+ * undefined
+ * object
+ * function
+ * 
+ * Tipos TS
+ * any (no usarlo)
+ * unknown
+ * never
+ * arrays
+ * tuplas
+ * Enums
+ * 
+ * tipo inferidos
+ */
+
+let extincionDinosaurios: number = 76_000_000;
+let dinosaurioFavorito: string = "T-rex";
+let extintos = true; 
