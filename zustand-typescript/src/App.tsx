da6c1213145f09b4fd4e7b7b39179e27d36a9cd8
Will_09 <wbahoque71@gmail.com>
2023-05-26 16:50:16 -0500

    Se empieza el proyecto de zustand con typescript

---
+++ zustand-typescript/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>Zustand App + TypeScript</div>
+    </>
+  )
+}
+
+export default App
