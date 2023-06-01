669d6a89105cad75ec4a883ec00d4a17774b38f3
Will_09 <wbahoque71@gmail.com>
2023-06-01 17:13:44 -0500

    Se muestra el json en pantalla

--- zustand-typescript/src/App.tsx
+++ zustand-typescript/src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useCounterStore } from './store/CounterStore';
 import { shallow } from 'zustand/shallow'; // Shallow, con esto podemos comparar un objeto con otro objeto
 
@@ -7,17 +8,39 @@ export const App = () => {
   // const count = useCounterStore((state) => state.count ); // Invocacion para manipulacion de datos
   // const title = useCounterStore((state) => state.title ); // Invocacion para manipulacion de datos
 
-  const { title, count} = useCounterStore((state) => ({
+  const { title, count, posts} = useCounterStore((state) => ({
 
     count: state.count,
     title: state.title,
+    posts: state.posts
 
   }), shallow)
+
+  // const increment = useCounterStore(state => state.increment); // 1RA FORMA DE ACCEDER AL INCREMENT
+  const { increment, getPosts } = useCounterStore();
+
+  const handleIncremento = () => {
+    increment(5);
+  }
+
+  useEffect(() => {
+    getPosts();
+  }, [])
 
   return (
     <>
       <div>
         <h1>{title}: {count}</h1>
+
+        <button
+          onClick={handleIncremento}
+        >
+          Increment by:5
+        </button>
+
+        <hr />
+
+        {JSON.stringify(posts)}
       </div>
     </>
   )
--- zustand-typescript/src/store/CounterStore.ts
+++ zustand-typescript/src/store/CounterStore.ts
@@ -1,13 +1,39 @@
 import create from 'zustand'; // Este create funciona como el useState de react
 
+
+interface Post {
+    id: number
+    title: string
+    body: string
+}
 interface CounterState {
     count: number
     title: string
+    posts: Post[]
+    increment: (value: number) => void
+    getPosts: () => Promise<void>
 }
 
-export const useCounterStore = create<CounterState>(() => ({
+export const useCounterStore = create<CounterState>((set) => ({
     count: 10,
-    title: "Some title"
+    title: "Some title",
+    posts: [],
+    increment: (value:number) => set(state => ({
+        count: state.count + value
+    })),
+    getPosts:async () => {
+        // const res = await fetch("https://jsonplaceholder.typicode.com/posts"); 1era forma de hacer una fetch
+        // const pots = await res.json();
+        // console.log(pots);
+
+        // 2da forma
+        const posts = await (await fetch("https://jsonplaceholder.typicode.com/posts")).json()
+        console.log(posts);
+        set(state => ({
+            ...state,
+            posts
+        }))
+    }
 }))
 
 // Este custom hook se hace para pasar en toda la app estados de manera global.
