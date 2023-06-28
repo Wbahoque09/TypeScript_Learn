a5abd53de573f5ce3f21a9853a58d8c98ecd9471
Will_09 <wbahoque71@gmail.com>
2023-06-28 16:20:32 -0500

    Se vio el tipo de dato array

---
+++ TypeScript-Fernando/bases/tipos/arrays.js
@@ -0,0 +1,9 @@
+"use strict";
+(() => {
+    // const numbers: (string | number | boolean )[] = [1,2,3,4,5,"6",7,8,9,10]; Aqui definimos varios tipos de datos
+    // numbers.push(true);
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    // console.log(numbers);
+    const villian = ['Omega Rojo', 'Dormammu', 'Duende Verde'];
+    villian.forEach(v => console.log(v.toUpperCase()));
+})();
