7af0cc569af447be416d4709cf30486a4da9b733
Will_09 <wbahoque71@gmail.com>
2023-06-28 15:11:44 -0500

    Se ve el tipo de dato any

---
+++ TypeScript-Fernando/bases/tipos/any.js
@@ -0,0 +1,11 @@
+"use strict";
+(() => {
+    let avenger = 123;
+    let exists;
+    let power;
+    avenger = "Dr. Strange";
+    // console.log(avenger.charAt(0));
+    console.log(avenger.charAt(0)); // as es un metodo que va a tratar avenger como string (casteo)
+    avenger = 150.2325415;
+    console.log(avenger.toFixed(2)); // el number entre mayor que y menor que es otro tipo de casteo
+})();
