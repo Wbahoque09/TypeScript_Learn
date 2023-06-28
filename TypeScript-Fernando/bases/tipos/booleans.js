6b9053779c3082ab383cbd144e1be91d95c651fd
Will_09 <wbahoque71@gmail.com>
2023-06-28 11:50:01 -0500

    Se ve el tipo de datos de booleano

---
+++ TypeScript-Fernando/bases/tipos/booleans.js
@@ -0,0 +1,7 @@
+"use strict";
+(() => {
+    let isSuperman = true;
+    let isBatman = false;
+    isSuperman = (isBatman) ? true : false; // validacion de ternario para asignar boolan
+    console.log(isSuperman);
+})();
---
+++ TypeScript-Fernando/bases/tipos/booleans.ts
@@ -0,0 +1,9 @@
+(() => {
+
+    let isSuperman:boolean = true;
+    let isBatman:boolean = false;
+    
+    isSuperman = ( isBatman ) ? true : false; // validacion de ternario para asignar boolan
+    console.log(isSuperman);
+
+})()
