d3ead2b21663ff1895e978b7fad82da102d0fef8
Will_09 <wbahoque71@gmail.com>
2023-06-28 09:18:22 -0500

    Se aprende conceptos y una breve introduccion sobre el modo observador de typescript

--- TypeScript-Fernando/bases/app.js
+++ TypeScript-Fernando/bases/app.js
@@ -1,3 +1,10 @@
 "use strict";
 const msg = "Hola mundo"; // Declaramos una constante y le damos el tipo de variable que queremos
-console.log(msg);
+const hero = {
+    name: "Iroman",
+    age: 45,
+};
+
+// hero.age = "50"; Empiezo cambiando una propiedad del objeto aqui en el js, no me da error convertir aqui esta propiedad del objeto
+
+console.log(hero.age + 1);
--- TypeScript-Fernando/bases/app.ts
+++ TypeScript-Fernando/bases/app.ts
@@ -2,4 +2,12 @@
 
 const msg: string = "Hola mundo"; // Declaramos una constante y le damos el tipo de variable que queremos
 
-console.log(msg);
+const hero = {
+    name: "Iroman",
+    age: 45,
+}
+
+// hero.age = "50"; Aqui si da error convertir la propiedad del objeto, pero cabe aclarar que si transpila aun habiendo error
+hero.age = 50; // Aqui si me vale la modificacion del objeto porque es el mismo tipo de dato 
+
+console.log(hero.age + 1);
