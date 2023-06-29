78eef517fb9438f97eed79419c0dc764a3022ecb
Will_09 <wbahoque71@gmail.com>
2023-06-29 09:50:01 -0500

    Se vio el tipo de dato en funciones void

---
+++ TypeScript-Fernando/bases/tipos/void.ts
@@ -0,0 +1,8 @@
+(() => {
+
+    function callBatman():void { // La funcion void retorna undefined, para asignat el tipo de dato que retorna una funcion se le asigna con dos puntos despues de los parentesis ():tipo_de_dato
+        
+    }
+    const a = callBatman();
+    console.log(a);
+})()
