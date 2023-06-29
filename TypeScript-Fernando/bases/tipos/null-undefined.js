ca6e9a71268c9c0fee468d18a3ede9691428c3e9
Will_09 <wbahoque71@gmail.com>
2023-06-29 10:50:40 -0500

    Se ven diferencias entre null y undefined

---
+++ TypeScript-Fernando/bases/tipos/null-undefined.js
@@ -0,0 +1,6 @@
+"use strict";
+(() => {
+    // let nada: undefined = undefined;
+    let nada = undefined; // De esta manera se puede definir variables de dos tipos de datos.
+    console.log(nada);
+})();
---
+++ TypeScript-Fernando/bases/tipos/null-undefined.ts
@@ -0,0 +1,9 @@
+
+(() => {
+
+    // let nada: undefined = undefined;
+    let nada: (boolean|undefined) = undefined; // De esta manera se puede definir variables de dos tipos de datos.
+
+    console.log(nada);
+
+})()
