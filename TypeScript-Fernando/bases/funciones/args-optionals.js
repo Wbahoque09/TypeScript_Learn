24217b62e83fb8661ecaa570b243a942f0d82be0
Will_09 <wbahoque71@gmail.com>
2023-06-29 15:45:23 -0500

    Se ve la parte de funciones que reciben parametros opcionales

---
+++ TypeScript-Fernando/bases/funciones/args-optionals.js
@@ -0,0 +1,8 @@
+"use strict";
+(() => {
+    const fullName = (firtName, lastName) => {
+        return `${firtName} ${lastName || ""}`;
+    };
+    const name = fullName("Tony", "Stank");
+    console.log({ name });
+})();
