4f8d311c2b58045af7a88bed2218dabd30821096
Will_09 <wbahoque71@gmail.com>
2023-07-04 09:18:05 -0500

    Se ve la parte de variables LET

--- TypeScript-Fernando/bases/ES6/let-var.ts
+++ TypeScript-Fernando/bases/ES6/let-var.ts
@@ -1,4 +1,14 @@
 
 (()=>{
-    
+
+    const nombre: string = "Fernando";
+
+    const getName = (): void => {
+        console.log("viejo getName");
+    }
+
+    // getName = () => { console.log('Nuevo getName') };
+    // getName()
+
+
 })()
