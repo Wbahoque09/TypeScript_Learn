46a431b2b0346b2afa2e6151bb802ea2b5dcb5ef
Will_09 <wbahoque71@gmail.com>
2023-07-04 15:56:24 -0500

    Se ve nuefa forma de asignar propiedades

--- TypeScript-Fernando/bases/classes/basica.ts
+++ TypeScript-Fernando/bases/classes/basica.ts
@@ -3,20 +3,27 @@
 
     class Avenger { // Creacion de la clase
 
-        private name: string = ""; // Se debe definir tipo de metodo de las propiedades, con el private solo se accede dentro de la clase
-        public team: string= "";
-        public realName?: string = "";
+        // private name: string = ""; // Se debe definir tipo de metodo de las propiedades, con el private solo se accede dentro de la clase
+        // public team: string= "";
+        // public realName?: string = "";
         static avAge: number = 35; // Las clases static no se pueden modificar y se acceden a ellas mediante nombre de la clase y la propiedad
 
-        constructor(name: string, team: string, realName?:string) {
-            this.name = name;
-            this.team = team;
-            this.realName = realName;
+        constructor(
+            private name: string, // Nueva forma de declarar las propiedades con su tipo de metodo
+            private team: string, 
+            public realName?:string,
+            avgAge: number = 55,
+            ) {
+                Avenger.avAge = avgAge;
+            // this.name = name;
+            // this.team = team;
+            // this.realName = realName; Forma tradicional
         }
 
     }
 
-    const antman: Avenger = new Avenger("Antman","Capitan");
+    const antman: Avenger = new Avenger("Antman","Capitan","Scott Lang");
+    // console.log(antman);
     // console.log( Avenger.avAge ); 
 
 })()
--- TypeScript-Fernando/bases/main.js
+++ TypeScript-Fernando/bases/main.js
@@ -1,16 +1,14 @@
 "use strict";
 (() => {
     class Avenger {
-        constructor(name, team, realName) {
-            this.name = "";
-            this.team = "";
-            this.realName = "";
+        constructor(name, team, realName, avgAge = 55) {
             this.name = name;
             this.team = team;
             this.realName = realName;
+            Avenger.avAge = avgAge;
         }
     }
     Avenger.avAge = 35;
-    const antman = new Avenger("Antman", "Capitan");
+    const antman = new Avenger("Antman", "Capitan", "Scott Lang");
 })();
 //# sourceMappingURL=main.js.map
