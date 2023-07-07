63a1496900b30fbaac079c4d79cb137381077eec
Will_09 <wbahoque71@gmail.com>
2023-07-07 10:59:08 -0500

    Se instala las dependencias de axios, y se crea una funcion flecha para la peticion

--- TypeScript-Fernando/ts-node/src/index.ts
+++ TypeScript-Fernando/ts-node/src/index.ts
@@ -1,4 +1,10 @@
+import { getPokemon } from './generic/get-pokemon';
 
+
+getPokemon(3)
+    .then( resp => console.log( resp ) )
+    .catch( error => console.error( error ) )
+    .finally( () => console.log("Fin de getPokemon") )
 
 
 
