360502c911a89a386fff503a8e2e45b2a74c9330
Will_09 <wbahoque71@gmail.com>
2023-07-07 17:01:03 -0500

    Terminar Ejemplos de decoradores

--- TypeScript-Fernando/ts-node/src/decorators/pokemon-class.ts
+++ TypeScript-Fernando/ts-node/src/decorators/pokemon-class.ts
@@ -1,8 +1,21 @@
-function printToConsole( constructor: Function ) {
+function printToConsole( constructor: Function ) { // Funcion que ejecuta decorador
     console.log(constructor)
 }
 
-@printToConsole
+const printToConsoleConditional = ( print: boolean = false ):Function => { // Se recibe un parametro tipo boolean
+    if ( print ) { // Se verifica el parametro recibido 
+        return printToConsole;
+    }
+    return () => {}
+}
+
+const bloquarPropotipo = function( constructor: Function ) {
+    Object.seal( constructor )
+    Object.seal( constructor.prototype );
+}
+
+@bloquarPropotipo
+@printToConsoleConditional( true ) // decorador, para su creacion se pone el arroba y se escribe el nombre de la funcion decoradora
 export class Pokemon {
 
     public publicApi: string = 'https://pokeapi.co';
