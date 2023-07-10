7dd0ea7df7e0a8ef99f66a92872d421c1ba883e9
Will_09 <wbahoque71@gmail.com>
2023-07-10 10:34:15 -0500

    Se termina la seccion de Rest Appi

--- TypeScript-Fernando/express-api/index.ts
+++ TypeScript-Fernando/express-api/index.ts
@@ -1,12 +1,21 @@
-const express = require('express')
+import express from 'express';
+
+// const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.json({
-        ok: true,
-        msg: 'Todo salio bien!'
+
+    res.status(401).json({
+        ok:false,
+        msg: 'No hay token en la peticion'
     })
+
+
+    // res.json({
+    //     ok: true,
+    //     msg: 'Todo salio bien!'
+    // })
 })
 
 app.listen(port, () => {
